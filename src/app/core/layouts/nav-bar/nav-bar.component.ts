344e0aa43267b1d1b0077cb989275b7ec26d7091
Carol-Rocha <caroldsr1@gmail.com>
2024-06-07 14:10:46 -0300

    feat: criar componente de navegacao e adicionar icons

--- src/app/core/layouts/nav-bar/nav-bar.component.ts
+++ src/app/core/layouts/nav-bar/nav-bar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
